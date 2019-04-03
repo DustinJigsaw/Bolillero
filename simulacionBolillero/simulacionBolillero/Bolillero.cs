bcf3e85571bba83ff2c3f65734c1ce4b8feb685f
DustinEscobar <dustingutierrez99@gmail.com>
2019-04-03 14:23:55 -0300

    1er Commit

---
+++ simulacionBolillero/simulacionBolillero/Bolillero.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simulacionBolillero
+{
+    public class Bolillero
+    {
+        public List<byte> BollillasAdentro { get; set; }
+        public List<byte> BollillasAfuera { get; set; }
+
+        Random r;
+         public Bolillero()
+        {
+            r = new Random(DateTime.Now.Millisecond);
+        }
+        public int IndiceAlAzar()
+        {
+            BollillasAdentro = (byte)r.Next(1, this.BollillasAfuera.Count + 1);
+        }
+
+        public byte SacarBolilla()
+        {
+            BollillasAdentro.ForEach(
+        }
+
+    }
+}
