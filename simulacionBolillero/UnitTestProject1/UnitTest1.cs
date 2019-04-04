722d7571156f00402b0af0138760192cfea6fc51
DustinEscobar <dustingutierrez99@gmail.com>
2019-04-04 17:39:57 -0300

    4to commit

---
+++ simulacionBolillero/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using simulacionBolillero;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Bolillero bolillero;
+        Simulacion simulacion;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            bolillero = new Bolillero(10, 1);
+
+            simulacion = new Simulacion();
+           
+           
+        }
+    }
+}
